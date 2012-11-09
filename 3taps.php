32d553220e95711e729adecb4bfd93b40daf5cbf
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2012-11-08 19:56:36 -0500

    Updated library to support v2.0 of the 3taps search API; removed API calls previously exposed by the API that need to be reconsidered server-side.

--- 3taps.php
+++ 3taps.php
@@ -2,8 +2,8 @@
 class threeTapsClient {
 	public static $clients = array();
 
-	public $authId;
-	public $host = 'api.3taps.com';
+	public $authToken;
+	public $host = '3taps.net';
 	public $response;
 	public $debug = false; // If set to true, will print out requests to error_log()
 
@@ -12,8 +12,8 @@ class threeTapsClient {
 		self::$clients[$type] = $client;
 	}
 
-	public function __construct($authId) {
-		$this->authId = $authId;
+	public function __construct($authToken) {
+		$this->authToken = $authToken;
 		
 		foreach (self::$clients as $type => $client) {
 			$this->$type = new $client($this);
@@ -24,12 +24,12 @@ class threeTapsClient {
 		$url = $path . $method;
 
 		if (!empty($getParams) || (empty($getParams) && empty($postParams))) {
-			$getParams['authID'] = $this->authId;
+			$getParams['authToken'] = $this->authToken;
 			$url .= '?' . http_build_query($getParams);
 		}
 		
 		if (!empty($postParams)) {
-			$postParams['authID'] = $this->authId;
+			$postParams['authToken'] = $this->authToken;
 			$post = http_build_query($postParams);
 		} else {
 			$post = null;
@@ -120,41 +120,20 @@ class threeTapsClient {
 	}
 }
 
-class threeTapsGeocoderClient {
-	public $client;
-	public $path = '/geocoder/';
-	
-	public function __construct($authId) {
-		if (is_a($authId, 'threeTapsClient')) {
-			$this->client = $authId;
-		} else {
-			$this->client = new threeTapsClient($authId);
-		}
-	}
-	
-	public function geocode($data) {
-		return $this->client->request($this->path, 'geocode', null, array('data' => json_encode($data)));
-	}
-}
-
-threeTapsClient::register('geocoder', 'threeTapsGeocoderClient');
-
 class threeTapsReferenceClient {
 	public $client;
 	public $path = '/reference/';
 	
-	public function __construct($authId) {
-		if (is_a($authId, 'threeTapsClient')) {
-			$this->client = $authId;
+	public function __construct($authToken) {
+		if (is_a($authToken, 'threeTapsClient')) {
+			$this->client = $authToken;
 		} else {
-			$this->client = new threeTapsClient($authId);
+			$this->client = new threeTapsClient($authToken);
 		}
 	}
 	
-	public function category($category_id = null) {
-		$method = 'category';
-		if (!empty($category_id)) $method .= '/' . $category_id;
-		$response = $this->client->request($this->path, $method, null, null);
+	public function categories() {
+		$response = $this->client->request($this->path, 'categories', null, null);
 		
 		if (!empty($category_id) && !empty($response)) {
 			return $response[0];
@@ -162,58 +141,19 @@ class threeTapsReferenceClient {
 		
 		return $response;
 	}
-	
-	public function location() {
-		return $this->client->request($this->path, 'location', null, null);
-	}
-	
-	public function source() {
-		return $this->client->request($this->path, 'source/get', null, null);
-	}
 }
 
 threeTapsClient::register('reference', 'threeTapsReferenceClient');
-
-class threeTapsPostingClient {
-	public $client;
-	public $path = '/posting/';
-	
-	public function __construct($authId) {
-		if (is_a($authId, 'threeTapsClient')) {
-			$this->client = $authId;
-		} else {
-			$this->client = new threeTapsClient($authId);
-		}
-	}
-	
-	public function create($data) {
-		return $this->client->request($this->path, 'create', null, array('postings' => json_encode($data)));
-	}
-	
-	public function delete($data) {
-		return $this->client->request($this->path, 'delete', null, array('data' => json_encode($data)));
-	}
-	
-	public function get($postKey) {
-		return $this->client->request($this->path, 'get/' . $postKey, null, null);
-	}
-	
-	public function update($data) {
-		return $this->client->request($this->path, 'update', null, array('data' => json_encode($data)));
-	}
-}
-
-threeTapsClient::register('posting', 'threeTapsPostingClient');
 
 class threeTapsSearchClient {
 	public $client;
 	public $path = '/search/';
 	
-	public function __construct($authId) {
-		if (is_a($authId, 'threeTapsClient')) {
-			$this->client = $authId;
+	public function __construct($authToken) {
+		if (is_a($authToken, 'threeTapsClient')) {
+			$this->client = $authToken;
 		} else {
-			$this->client = new threeTapsClient($authId);
+			$this->client = new threeTapsClient($authToken);
 		}
 	}
 
@@ -221,12 +161,8 @@ class threeTapsSearchClient {
 		return $this->client->request($this->path, 'count', $params, null);
 	}
 	
-	public function range($params) {
-		return $this->client->request($this->path, 'range', $params, null);
-	}
-	
 	public function search($params) {
-		return $this->client->request($this->path, 'search', $params, null);
+		return $this->client->request($this->path, '', $params, null);
 	}
 	
 	public function summary($params) {
@@ -236,29 +172,3 @@ class threeTapsSearchClient {
 
 threeTapsClient::register('search', 'threeTapsSearchClient');
 
-class threeTapsStatusClient {
-	public $client;
-	public $path = '/status/';
-	
-	public function __construct($authId) {
-		if (is_a($authId, 'threeTapsClient')) {
-			$this->client = $authId;
-		} else {
-			$this->client = new threeTapsClient($authId);
-		}
-	}
-	
-	public function get($postings) {
-		return $this->client->request($this->path, 'get', null, array('postings' => json_encode($postings)));
-	}
-	
-	public function system() {
-		return $this->client->request($this->path, 'system', null, null);
-	}
-	
-	public function update($postings) {
-		return $this->client->request($this->path, 'update', null, array('postings' => json_encode($postings)));
-	}
-}
-
-threeTapsClient::register('status', 'threeTapsStatusClient');
--- 3taps_test.php
+++ 3taps_test.php
@@ -1,6 +1,6 @@
 <?
 
-DEFINE('API_KEY', 'jmrfhu59cnmtnzusshd62pbg');
+DEFINE('API_KEY', '75c8e3851e874fe692a8199022a06645');
 
 require('3taps.php'); 
 require('simpletest/autorun.php'); 
@@ -12,7 +12,7 @@ class TestOfSearchClient extends UnitTestCase {
 	private $params = array(
 			'rpp' => 4,
 			'text' => 'honda',
-			'source' => 'EBAYM' // eBay Motors
+			'source' => 'CRAIG'
 	);
 
 	function __construct() {
@@ -36,13 +36,6 @@ class TestOfSearchClient extends UnitTestCase {
 		$result = $this->client->search->count($this->params);
 		$this->assertTrue(count($result['count']) > 0);
 	}
-
-	function testRange() {
-		$count_params = $this->params;
-		$count_params['fields'] = 'price';
-		$result = $this->client->search->range($count_params);
-		$this->assertTrue(count($result['price']) > 0);
-	}
 }
 
 class TestOfReferenceClient extends UnitTestCase {
@@ -56,152 +49,9 @@ class TestOfReferenceClient extends UnitTestCase {
 		$this->client->debug = true;
 	}
 	
-	function testCategory() {
-		$results = $this->client->reference->category();
-		$this->assertTrue(count($results) > 0);
-
-		$results = $this->client->reference->category('VAUT');
-		$this->assertTrue(count($results) > 0);
-	}
-
-	function testLocation() {
-		$results = $this->client->reference->location();
-		$this->assertTrue(count($results) > 0);
-	}
-
-	function testSource() {
-		$results = $this->client->reference->source();
+	function testCategories() {
+		$results = $this->client->reference->categories();
 		$this->assertTrue(count($results) > 0);
 	}
 }
 
-class TestOfGeocoderClient extends UnitTestCase {
-
-	private $client;
-
-	function __construct() {
-		parent::__construct('3taps Geocoder API test');
-
-		$this->client = new threeTapsClient(API_KEY);
-		$this->client->debug = true;
-	}
-	
-	function testGeocode() {
-		$locations = array(
-			array(
-				'text' => '90210'
-			)
-		);
-		$results = $this->client->geocoder->geocode($locations);
-		$this->assertTrue(count($results) > 0);
-	}
-}
-
-class TestOfStatusClient extends UnitTestCase {
-
-	private $client;
-
-	function __construct() {
-		parent::__construct('3taps Status API test');
-
-		$this->client = new threeTapsClient(API_KEY);
-		$this->client->debug = true;
-	}
-	
-	function testSystem() {
-		$result = $this->client->status->system();
-		$this->assertTrue($result['code'] == '200');
-	}
-
-	function testUpdate() {
-		$postings = array(
-			array(
-				'status' => 'found',
-				'externalID' => 'NOTANID',
-				'source' => 'E_BAY'
-			)
-		);
-		$result = $this->client->status->update($postings);
-		$this->assertTrue($result['code'] == '200');
-	}
-
-	function testGet() {
-		$postings = array(
-			array(
-				'externalID' => 'NOTANID',
-				'source' => 'E_BAY'
-			)
-		);
-		$results = $this->client->status->get($postings);
-		$this->assertTrue(count($results));
-		$this->assertTrue($results[0]['exists'] == true);
-	}
-}
-
-class TestOfPostingClient extends UnitTestCase {
-
-	private $client;
-
-	private $postKeys = array();
-
-	function __construct() {
-		parent::__construct('3taps Posting API test');
-
-		$this->client = new threeTapsClient(API_KEY);
-		$this->client->debug = true;
-	}
-	
-	function testCreate() {
-		$postings = array(
-			array(
-				'source' => '3TAPS',
-				'category' => 'VAUT',
-				'heading' => 'This is a test posting.',
-				'location' => 'LAX',
-				'timestamp' => date('Y/m/d h:i:s')
-			),
-			array(
-				'source' => '3TAPS',
-				'category' => 'VAUT',
-				'heading' => 'This is a test posting.',
-				'location' => 'LAX',
-				'timestamp' => date('Y/m/d h:i:s')
-			)
-		);
-
-		$results = $this->client->posting->create($postings);
-		$this->postKeys = array($results[0]['postKey'], $results[0]['postKey']);
-
-		$this->assertTrue(count($results) == 2);
-		$this->assertTrue($results[0]['postKey']);
-		$this->assertTrue(!isset($results[0]['error']));
-		$this->assertTrue($results[1]['postKey']);
-		$this->assertTrue(!isset($results[1]['error']));
-	}
-
-	function testGet() {
-		$result = $this->client->posting->get($this->postKeys[0]);
-		$this->assertTrue($result);
-	}
-
-	function testUpdate() {
-		$postings = array(
-			array(
-				$this->postKeys[0],
-				array(
-					'heading' => 'Updated Heading'
-				)
-			)
-		);
-		
-		$result = $this->client->posting->update($postings);
-		$this->assertTrue($result['success']);
-	}
-
-	function testDelete() {
-		$result = $this->client->posting->delete($this->postKeys);
-		$this->assertTrue($result['success']);
-	}
-
-	
-}
